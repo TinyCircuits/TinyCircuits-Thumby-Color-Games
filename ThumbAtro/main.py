27563c336b816507289fc28e069b467e6ee23e8e
DeMatt <DeMatt@users.noreply.github.com>
2025-07-25 11:33:24 -0600

    End rumble if not playing sound.

--- ThumbAtro/main.py
+++ ThumbAtro/main.py
@@ -624,6 +624,7 @@ class PokerGame(Rectangle2DNode):
     def toggle_volume(self):
         if self.volume == .25:
             self.volume = 0
+            engine_io.rumble(0)
         else:
             self.volume = .25
         engine_audio.set_volume(self.volume)
@@ -646,6 +647,7 @@ class PokerGame(Rectangle2DNode):
         self.last_sound_time = None
         self.sound_play_duration = 0
         self.sound_start_time = None
+        engine_io.rumble(0)
 
     def tick(self, dt):
         if engine_io.MENU.is_just_pressed:
